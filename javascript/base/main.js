8730640ccf8ac76708fda9a7c76f649187827621
Jeffrey Wilcke <stygeo@gmail.com>
2011-09-10 15:28:09 +0200

    Added fake save and __bind to get the right context

--- javascript/base/main.js
+++ javascript/base/main.js
@@ -1,24 +1,38 @@
 $(function() {
+  var __bind = function(fn, me){ return function(){ return fn.apply(me, arguments); }; };
+
   File = (function() {
     function File(path) {
       this.path = path;
       this.net  = require("sprock/net");
+      this.content = null;
 
-      this.net.get('/load_file?path='+path, function(p_content) {
-        // This is 'window'
-        this.m_content = p_content;
-      });
+      if(this.path) {
+        this.net.get('/load_file?path='+path, function(p_content) {
+          // This is 'window'
+          this.m_content = p_content;
+        });
 
-      this.content = window.m_content;
+        this.content = window.m_content;
+      }
     }
 
+    // Return the content of the file
     File.prototype.read = function() {
       return this.content;
     };
 
-    File.prototype.file_type = function() {
-      this.content.split(".")[this.content.length-1];
+    // Save the file
+    File.prototype.save = function() {
+      console.log("Fake save");
+      console.log(this.content);
     }
+
+    // Return the file type
+    File.prototype.file_type = function() {
+      var split = this.path.split(".");
+      return split[split.length-1];
+    };
 
     return File;
   })();
@@ -28,24 +42,38 @@ $(function() {
     function Editor(element_name) {
       // All available modes
       this.modes = require('sprock/syntax').Syntax;
-      // Current mode
-      this.mode = null;
-
       // All available themes
       this.themes = require('sprock/themes').Themes;
+      // Canon event handler
+      this.canon = require('pilot/canon');
+
+      // Current mode
+      this.mode = null;
       // Current theme
       this.theme = "";
-
+      // Current file
+      this.file = null;
 
       this.EditSession  = require("ace/edit_session").EditSession;
       this.UndoManager  = require("ace/undomanager").UndoManager;
 
       this.editor = ace.edit(element_name);
-      this.session = this.open_file('/Users/stygeo/development/ace/application.rb');
+      this.open_file('/Users/stygeo/development/sprock/javascript/base/main.js');
 
       // Temp
       this.editor.setTheme(this.themes.twilight);
-      this.editor.setSession(this.session);
+
+      this.canon.addCommand({
+        name: 'save',
+        bindKey: {
+          win: "Ctrl-S",
+          mac: "Command-S",
+          sender: "editor"
+        },
+        exec: __bind(function(editor) {
+          this.save_file();
+        }, this)
+      });
     }
 
     // Set the current mode of the editor (Html, js, Ruby, etc)
@@ -56,20 +84,21 @@ $(function() {
 
     // Get the current mode
     Editor.prototype.get_mode = function() {
-      this.mode;
+      return this.mode;
     };
 
     // Set the current theme
     Editor.prototype.set_theme = function(theme) {
       this.theme = theme;
       this.editor.getSession().setTheme(theme);
-    }
+    };
 
     // Get the current theme
     Editor.prototype.get_theme = function() {
-      this.theme;
-    }
+      return this.theme;
+    };
 
+    // Open the file and return the session
     Editor.prototype.open_file = function(path) {
       var EditSession = require("ace/edit_session").EditSession;
       var file = new File(path);
@@ -79,11 +108,23 @@ $(function() {
         case 'rb':
           session.setMode(this.modes.RubyMode);
           break;
+        case 'js':
+          session.setMode(this.modes.JavascriptMode);
+          break;
+        // TODO Add more modes
       }
+      this.file = file;
 
-      session.setMode(this.modes.RubyMode);
-      return session;
-    }
+      this.session = session;
+      this.editor.setSession(this.session);
+
+      return this.session;
+    };
+
+    Editor.prototype.save_file = function() {
+      this.file.content = this.editor.getSession().getValue();
+      this.file.save();
+    };
 
     return Editor;
   })();
@@ -104,6 +145,6 @@ $(function() {
     return Sprock;
   })();
 
-  new Sprock;
+  new Sprock();
 
 });
